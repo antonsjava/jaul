d21898a527906672815ab7e5700bf74e2f60f51f
Anton Straka <anton_straka@datalan.sk>
2015-10-12 10:01:36 +0200

    add TextContet methods

--- src/main/java/sk/antons/jaul/xml/EW.java
+++ src/main/java/sk/antons/jaul/xml/EW.java
@@ -380,5 +380,29 @@ public class EW {
             
         }
     }
+    
+
+    /**
+     * return text content of first descendant of this node filtered by tag name
+     * @param tag - tag name of selected children
+     * @return selected element
+     */
+    public String firstElementByTagNameTextContent(String tag) {
+        EW ew = traverseFirst(element, null, tag);
+        if(ew == null) return null;
+        return ew.elem().getTextContent();
+    }
+
+    /**
+     * return text content of first descendant of this node filtered by tag name
+     * @param ns - namespace prefix of tag name
+     * @param tag - tag name of selected children
+     * @return selected element
+     */
+    public String firstElementByTagNameNSTextContent(String ns, String tag) {
+        EW ew = traverseFirst(element, ns, tag);
+        if(ew == null) return null;
+        return ew.elem().getTextContent();
+    }
 
 }
