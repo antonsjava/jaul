78cb7a1b102355d7d0b4493f7a05812a382ae15e
Anton Straka <anton_straka@datalan.sk>
2016-02-25 19:53:52 +0100

    add Unicode and Cml escape methods

---
+++ src/main/java/sk/antons/jaul/binary/Unicode.java
@@ -0,0 +1,243 @@
+/*
+ * Copyright 2015 Anton Straka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sk.antons.jaul.binary;
+
+import sk.antons.jaul.Is;
+
+/**
+ * Converts String chars to escaped (\u0000) form string and vice versa.
+ * Only visible ASCII chars leave unchanged. 
+ * 
+ * @author antons
+ */
+public class Unicode {
+    
+	/**
+	 * Translates the given String into ASCII chars and others are escaped.
+	 * 
+	 * @param input - string to be encoded
+	 * @param ignoreInvisible - chars up to 31 will be not escaped (useful if
+     *                          you want to keep new lines and tabs)
+	 * @return new String with escaped non visible ASII chars
+	 */
+	public static String escape(String input, boolean ignoreInvisible) {
+		if(Is.empty(input)) return input;
+		StringBuilder sb = new StringBuilder(input.length() + 30);
+		for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if((ignoreInvisible || (c > 31)) && (c < 127)) { 
+                sb.append(c);
+            } else {
+            	sb.append("\\u");
+            	String hex = Integer.toHexString(c);
+            	for(int j = hex.length(); j < 4; j++) sb.append( '0' );
+            	sb.append( hex );
+            }
+        }
+		return sb.toString();
+	}
+	
+	
+	/**
+	 * Decodes unicode escaped literal back to real String.
+	 * 
+	 * @param input text with escape sequences
+	 * @return String with translated escape sequences
+	 */
+	public static String unescape(String input) {
+		if(Is.empty(input)) return input;
+		StringBuilder sb = new StringBuilder( input.length() );
+		boolean isbs = false;
+		for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (isbs) {
+            	isbs = false;
+                if(c == 'f')  c = '\f';
+                else if(c == 'n') c = '\n';
+                else if(c == 'r') c = '\r';
+                else if(c == 't') c = '\t';
+                else if(c == '\\') c = '\\';
+                else if(c == 'u') {
+                    String hex = input.substring( i + 1, i + 5 );
+                    c = (char) Integer.parseInt(hex, 16 );
+                    i += 4;
+                } else {
+                    sb.append('\\');
+                }
+            } else {
+            	isbs = (c == '\\');
+            }
+            if (!isbs) sb.append(c);
+        }
+		return sb.toString();
+	}
+
+    /**
+	 * Translates the given String into ASCII chars and others are escaped.
+     * excape also \r, \n. ... and other Java special characters
+     * @param input string to be escaped
+     * @return escaped string
+     */
+	public static String escapeJava(String input) {
+		if(Is.empty(input)) return input;
+		StringBuilder sb = new StringBuilder(input.length() + 30);
+		for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if(c == '\b') sb.append("\\b");
+            else if(c == '\n') sb.append("\\n");
+            else if(c == '\r') sb.append("\\r");
+            else if(c == '\t') sb.append("\\t");
+            else if(c == '\f') sb.append("\\f");
+            else if(c == '\'') sb.append("\\\'");
+            else if(c == '"') sb.append("\\\"");
+            else if((c > 31) && (c < 127)) sb.append(c);
+            else {
+            	sb.append("\\u");
+            	String hex = Integer.toHexString(c);
+            	for(int j = hex.length(); j < 4; j++) sb.append( '0' );
+            	sb.append( hex );
+            }
+        }
+		return sb.toString();
+	}
+    
+	/**
+	 * Decodes unicode escaped literal back to real String.
+     * unexape also Java special characters like \n \r, ...
+	 * 
+	 * @param input text with escape sequences
+	 * @return String with translated escape sequences
+	 */
+	public static String unescapeJava(String input) {
+		if(Is.empty(input)) return input;
+		StringBuilder sb = new StringBuilder( input.length() );
+		boolean isbs = false;
+		for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (isbs) {
+            	isbs = false;
+                if(c == 'f')  c = '\f';
+                else if(c == 'n') c = '\n';
+                else if(c == 'r') c = '\r';
+                else if(c == 't') c = '\t';
+                else if(c == 'b') c = '\b';
+                else if(c == '\\') c = '\\';
+                else if(c == '"') c = '"';
+                else if(c == '\'') c = '\'';
+                else if(c == 'u') {
+                    String hex = input.substring( i + 1, i + 5 );
+                    c = (char) Integer.parseInt(hex, 16 );
+                    i += 4;
+                } else {
+                    sb.append('\\');
+                }
+            } else {
+            	isbs = (c == '\\');
+            }
+            if (!isbs) sb.append(c);
+        }
+		return sb.toString();
+	}
+
+
+	/**
+	 * Translates the given String into ASCII chars and others are escaped.
+     * Uses HTML &#XXX; escape form;
+	 * 
+	 * @param input - string to be encoded
+	 * @param ignoreInvisible - chars up to 31 will be not escaped (useful if
+     *                          you want to keep new lines and tabs)
+	 * @return new String with escaped non visible ASII chars
+	 */
+	public static String escapeHtml( String input, boolean ignoreInvisible) {
+		if(Is.empty(input)) return input;
+        StringBuilder buff = new StringBuilder();
+        for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if((ignoreInvisible || (c > 31)) && (c < 127)) buff.append(c);
+            else buff.append("&#").append((int)c).append(';');
+        }
+        return buff.toString();
+	}
+	
+	/**
+	 * Decodes unicode escaped literal back to real String.
+     * Uses HTML &#XXX; escape form;
+	 * 
+	 * @param input text with escape sequences
+	 * @return String with translated escape sequences
+	 */
+	public static String unescapeHtml(String input) {
+		if(Is.empty(input)) return input;
+        StringBuilder buff = new StringBuilder();
+/*
+0 - none
+1 - and
+2 - cross
+3 - num
+4 - ;
+ */
+        int state = 0;
+        int sbuff = 0;
+        for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if(state == 0) {
+                if(c == '&') {
+                    state = 1;
+                    continue;
+                }
+            } else if(state == 1) {
+                if(c == '#') {
+                    state = 2;
+                    sbuff = 0;;
+                    continue;
+                } else {
+                    buff.append('&');
+                    state = 0;
+                }
+            } else if(state == 2) {
+                if(('0' <= c) && (c <= '9')) {
+                    state = 3;
+                    sbuff = 10*sbuff + (c - '0');
+                    continue;
+                } else {
+                    buff.append('&');
+                    buff.append('#');
+                    state = 0;
+                }
+            } else if(state == 3) {
+                if(('0' <= c) && (c <= '9')) {
+                    state = 3;
+                    sbuff = 10*sbuff + (c - '0');
+                    continue;
+                } else if((c == ';')) {
+                    buff.append((char)sbuff);
+                    state = 0;
+                    continue;
+                } else {
+                    buff.append('&');
+                    buff.append('#');
+                    buff.append(sbuff);
+                    state = 0;
+                }
+            } 
+            buff.append(c);
+        }
+        return buff.toString();
+	}
+    
+}
--- src/main/java/sk/antons/jaul/xml/Xml.java
+++ src/main/java/sk/antons/jaul/xml/Xml.java
@@ -24,6 +24,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
+import sk.antons.jaul.Is;
 
 /**
  * Helper class for creating xml document. Simplify document creation and maps 
@@ -106,5 +107,104 @@ public class Xml {
             throw new IllegalArgumentException("Unable to parse xml document from file " + file, e);
         }
     }
+
+    /**
+     * Escapes "'<>& characters to XML compliant escape sequences.
+     * If you handle also &000; sequences use also Unicode.escapeHtml() method.
+     * @param value to be escaped
+     * @return escaped value
+     */
+    public static String escape(String value) {
+        if(Is.empty(value)) return value;
+        StringBuilder sb = new StringBuilder();
+        int len = value.length();
+        for(int i = 0; i < len; i++) {
+            char c = value.charAt(i);
+            if(c == '\"') sb.append("&quot;");
+            else if(c == '\'') sb.append("&apos;");
+            else if(c == '<') sb.append("&lt;");
+            else if(c == '>') sb.append("&gt;");
+            else if(c == '&') sb.append("&amp;");
+            else sb.append(c);
+        }
+        return sb.toString();
+    }
+    
+    /**
+     * Escapes XML compliant escape sequences into "'<>& characters.
+     * If you handle also &000; sequences use also Unicode.unescapeHtml() method.
+     * @param value escaped value
+     * @return un-escaped value
+     */
+    public static String unescape(String value) {
+/*
+  0 - none
+  1 - &
+  2 - &q
+  3 - &qu
+  4 - &quo
+  5 - &quot
+  6 - &l
+  7 - &lt
+  8 - &g
+  9 - &gt
+  10 - &a
+  11 - &ap
+  12 - &apo
+  13 - &apos
+  14 - &am
+  15 - &amp
+*/
+
+        if(Is.empty(value)) return value;
+        StringBuilder sb = new StringBuilder();
+        int len = value.length();
+        int state = 0;
+        for(int i = 0; i < len; i++) {
+            char c = value.charAt(i);
+            if(c == '&') state = 1;
+            else if((c == 'q') && (state == 1)) state = 2;
+            else if((c == 'u') && (state == 2)) state = 3;
+            else if((c == 'o') && (state == 3)) state = 4;
+            else if((c == 't') && (state == 4)) state = 5;
+            else if((c == 'l') && (state == 1)) state = 6;
+            else if((c == 't') && (state == 6)) state = 7;
+            else if((c == 'g') && (state == 1)) state = 8;
+            else if((c == 't') && (state == 8)) state = 9;
+            else if((c == 'a') && (state == 1)) state = 10;
+            else if((c == 'p') && (state == 10)) state = 11;
+            else if((c == 'o') && (state == 11)) state = 12;
+            else if((c == 's') && (state == 12)) state = 13;
+            else if((c == 'm') && (state == 10)) state = 14;
+            else if((c == 'p') && (state == 14)) state = 15;
+            else if((c == ';') && (state == 15)) {sb.append('&'); state = 0; }
+            else if((c == ';') && (state == 13)) {sb.append('\''); state = 0; }
+            else if((c == ';') && (state == 9)) {sb.append('>'); state = 0; }
+            else if((c == ';') && (state == 7)) {sb.append('<'); state = 0; }
+            else if((c == ';') && (state == 5)) {sb.append('\"'); state = 0; }
+            else {
+                if(state == 1) sb.append("&");
+                else if(state == 2) sb.append("&q");
+                else if(state == 3) sb.append("&qu");
+                else if(state == 4) sb.append("&quo");
+                else if(state == 5) sb.append("&quot");
+                else if(state == 6) sb.append("&l");
+                else if(state == 7) sb.append("&lt");
+                else if(state == 8) sb.append("&g");
+                else if(state == 9) sb.append("&gt");
+                else if(state == 10) sb.append("&a");
+                else if(state == 11) sb.append("&ap");
+                else if(state == 12) sb.append("&apo");
+                else if(state == 13) sb.append("&apos");
+                else if(state == 14) sb.append("&am");
+                else if(state == 15) sb.append("&amp");
+                state = 0;
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+
+    }
+
     
 }
